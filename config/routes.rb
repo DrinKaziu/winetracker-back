bc595adf4b06565be505314cb91984f27398a961
SnowSurfer <drinkaziu@gmail.com>
2018-07-15 17:26:30 -0400

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :winelists, only: [:index, :create, :destroy, :show, :update]
+      resources :wines, only: [:index, :show, :create, :destroy, :update]
+      resources :winelist_wines, only: [:index, :create, :destroy]
+    end
+  end
 end
